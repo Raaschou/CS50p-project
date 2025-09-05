3ade8535d74bd382aa01b3929efb7a301139b327
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-09-05 22:41:42 +0200

    Add parse date test

--- test_project.py
+++ test_project.py
@@ -1,8 +1,13 @@
+import pytest
+from project import parse_date
+
 def main():
     ...
 
 
-def ...
+def test_parse_date():
+    assert parse_date("12/10/2019") == "2019-12-10"
 
 
-if __name__
+if __name__ == "__main__":
+    main()
